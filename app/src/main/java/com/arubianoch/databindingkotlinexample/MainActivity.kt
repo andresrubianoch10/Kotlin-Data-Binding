9269e14564cf1184aac4bba3be6eca9409726d20
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-02-02 02:00:07 -0500

    Added databinding functionality

--- app/src/main/java/com/arubianoch/databindingkotlinexample/MainActivity.kt
+++ app/src/main/java/com/arubianoch/databindingkotlinexample/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.arubianoch.databindingkotlinexample
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.arubianoch.databindingkotlinexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //First Way
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+
+        binding.user = User("Andres", "Rubiano Del Chiaro")
     }
 }
---
+++ app/src/main/java/com/arubianoch/databindingkotlinexample/User.kt
@@ -0,0 +1,6 @@
+package com.arubianoch.databindingkotlinexample
+
+/**
+ * @author Andres Rubiano Del Chiaro
+ */
+data class User(val firstName: String, val lastName: String)
